f453d2906de8a8f6d618eea47c3af677c7efe28e
branscha <branscha@5ee35f29-4713-0410-8bc3-af33b2071d23>
2006-05-30 21:35:01 +0000

    First version of log4j JSON tool.

--- trunk/jsontools-log4j/src/main/java/com/sdicons/json/log4j/JSONLayout.java
+++ trunk/jsontools-log4j/src/main/java/com/sdicons/json/log4j/JSONLayout.java
@@ -1,14 +1,82 @@
 package com.sdicons.json.log4j;
 
+/*
+    JSONTools - Java JSON Tools
+    Copyright (C) 2006 S.D.I.-Consulting BVBA
+    http://www.sdi-consulting.com
+    mailto://nospam@sdi-consulting.com
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
 import org.apache.log4j.Layout;
+import org.apache.log4j.helpers.ISO8601DateFormat;
 import org.apache.log4j.spi.LoggingEvent;
+import org.apache.log4j.spi.ThrowableInformation;
+import com.sdicons.json.model.JSONObject;
+import com.sdicons.json.model.JSONString;
+import com.sdicons.json.model.JSONArray;
+import com.sdicons.json.model.JSONInteger;
+
+import java.util.Date;
+import java.text.DateFormat;
+import java.math.BigInteger;
 
 public class JSONLayout
 extends Layout
 {
     public java.lang.String format(LoggingEvent loggingEvent)
     {
-        return null;
+        DateFormat lFormat = new ISO8601DateFormat();
+
+        // First we create a JSON logging object.
+        JSONObject lLogObj = new JSONObject();
+        // Now we can fill the object with the event attributes.
+        lLogObj.getValue().put("level", new JSONString(loggingEvent.getLevel().toString()));
+        lLogObj.getValue().put("message", new JSONString(loggingEvent.getRenderedMessage()));
+        lLogObj.getValue().put("logger", new JSONString(loggingEvent.getLoggerName()));
+        lLogObj.getValue().put("thread", new JSONString(loggingEvent.getThreadName()));
+
+        JSONArray lLogTimes = new JSONArray();
+        lLogTimes.getValue().add(new JSONString(lFormat.format(new Date(loggingEvent.timeStamp))));
+        lLogTimes.getValue().add(new JSONInteger(new BigInteger(new Long(loggingEvent.timeStamp).toString())));
+        lLogObj.getValue().put("timestamp", lLogTimes);
+
+        ThrowableInformation lTi = loggingEvent.getThrowableInformation();
+        if(lTi != null)
+        {
+            JSONObject lExcObj = new JSONObject();
+            lLogObj.getValue().put("exception", lExcObj);
+
+            JSONArray lExcMsgs = new JSONArray();
+            String[] lThrRep = loggingEvent.getThrowableStrRep();
+            if(lThrRep != null && lThrRep.length > 0)
+            {
+                for (String lMsg : lThrRep)
+                {
+                    lExcMsgs.getValue().add(new JSONString(lMsg));
+                }
+
+            }
+
+            lExcObj.getValue().put("class", new JSONString(lTi.getThrowable().getClass().getCanonicalName()));
+            lExcObj.getValue().put("stacktrace", lExcMsgs);
+            lExcObj.getValue().put("message", new JSONString(lTi.getThrowable().getMessage()));
+        }
+
+        return lLogObj.render(true) + "\n";
     }
 
     public void activateOptions()
@@ -18,6 +86,6 @@ extends Layout
 
     public boolean ignoresThrowable()
     {
-        return false;  
+        return false;
     }
 }
---
+++ trunk/jsontools-log4j/src/test/java/com/sdicons/json/log4j/LayoutTest.java
@@ -0,0 +1,69 @@
+package com.sdicons.json.log4j;
+
+/*
+    JSONTools - Java JSON Tools
+    Copyright (C) 2006 S.D.I.-Consulting BVBA
+    http://www.sdi-consulting.com
+    mailto://nospam@sdi-consulting.com
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+import junit.framework.TestCase;
+import org.apache.log4j.*;
+
+import java.io.File;
+import java.io.IOException;
+
+
+public class LayoutTest
+extends TestCase
+{
+    public void testLayout()
+    {
+        File lTempLog = null;
+
+        try
+        {
+            lTempLog = File.createTempFile("json-log-test", ".txt");
+            lTempLog.createNewFile();
+
+            Logger lLog = Logger.getLogger(LayoutTest.class);
+
+            Layout lLayout = new JSONLayout();
+            FileAppender lAppender = new FileAppender();
+            lAppender.setFile(lTempLog.getAbsolutePath());
+            lAppender.setLayout(lLayout);
+            lAppender.activateOptions();
+
+            Logger lRootLogger = Logger.getRootLogger();
+            lRootLogger.addAppender(new ConsoleAppender(lLayout));
+            lRootLogger.addAppender(lAppender);
+
+            lLog.info("This is an info message.");
+            lLog.debug("This is a debug message.");
+            lLog.error("This is an error message.");
+            lLog.fatal("This is a fatal message.", new IllegalArgumentException("This is an illegal argument."));
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            if(lTempLog != null) lTempLog.delete();
+        }
+    }
+}

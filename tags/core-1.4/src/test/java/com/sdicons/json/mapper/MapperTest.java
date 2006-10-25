1327a4d6492807d2e457070cd1a2b11e25838ff7
branscha <branscha@5ee35f29-4713-0410-8bc3-af33b2071d23>
2006-10-25 20:01:18 +0000

    Version 1.4 of the core is a fact.

---
+++ tags/core-1.4/src/test/java/com/sdicons/json/mapper/MapperTest.java
@@ -0,0 +1,252 @@
+package com.sdicons.json.mapper;
+
+import com.sdicons.json.model.JSONValue;
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.LinkedHashMap;
+
+public class MapperTest
+extends TestCase
+{
+    public static class TestBean
+    {
+        private Boolean booleanMbr;
+        private String stringMbr;
+        private Integer integerMbr;
+        private int intMbr;
+
+        private Short shortMbr;
+        private Byte byteMbr;
+        private Long longMbr;
+        private Float floatMbr;
+        private Double doubleMbr;
+        private BigInteger bigIntMbr;
+        private BigDecimal bigDecimalMbr;
+        private Character charMbr;
+        private Date date;
+
+        private LinkedList<String> linkedList;
+        private ArrayList<Date> arrayList;
+
+        private LinkedHashMap<String, Date> linkedMap;
+
+        public Integer getIntegerMbr()
+        {
+            return integerMbr;
+        }
+
+        public void setIntegerMbr(Integer integerMbr)
+        {
+            this.integerMbr = integerMbr;
+        }
+
+        public Short getShortMbr()
+        {
+            return shortMbr;
+        }
+
+        public void setShortMbr(Short shortMbr)
+        {
+            this.shortMbr = shortMbr;
+        }
+
+        public String getStringMbr()
+        {
+            return stringMbr;
+        }
+
+        public void setStringMbr(String stringMbr)
+        {
+            this.stringMbr = stringMbr;
+        }
+
+        public Boolean getBooleanMbr()
+        {
+            return booleanMbr;
+        }
+
+        public void setBooleanMbr(Boolean booleanMbr)
+        {
+            this.booleanMbr = booleanMbr;
+        }
+
+        public Byte getByteMbr()
+        {
+            return byteMbr;
+        }
+
+        public void setByteMbr(Byte byteMbr)
+        {
+            this.byteMbr = byteMbr;
+        }
+
+        public long getLongMbr()
+        {
+            return longMbr;
+        }
+
+        public void setLongMbr(Long longMbr)
+        {
+            this.longMbr = longMbr;
+        }
+
+        public Double getDoubleMbr()
+        {
+            return doubleMbr;
+        }
+
+        public void setDoubleMbr(Double doubleMbr)
+        {
+            this.doubleMbr = doubleMbr;
+        }
+
+        public Float getFloatMbr()
+        {
+            return floatMbr;
+        }
+
+        public void setFloatMbr(Float floatMbr)
+        {
+            this.floatMbr = floatMbr;
+        }
+
+        public BigDecimal getBigDecimalMbr()
+        {
+            return bigDecimalMbr;
+        }
+
+        public void setBigDecimalMbr(BigDecimal bigDecimalMbr)
+        {
+            this.bigDecimalMbr = bigDecimalMbr;
+        }
+
+        public BigInteger getBigIntMbr()
+        {
+            return bigIntMbr;
+        }
+
+        public void setBigIntMbr(BigInteger bigIntMbr)
+        {
+            this.bigIntMbr = bigIntMbr;
+        }
+
+        public Character getCharMbr()
+        {
+            return charMbr;
+        }
+
+        public void setCharMbr(Character charMbr)
+        {
+            this.charMbr = charMbr;
+        }
+
+        public int getIntMbr()
+        {
+            return intMbr;
+        }
+
+        public void setIntMbr(int intMbr)
+        {
+            this.intMbr = intMbr;
+        }
+
+        public Date getDate()
+        {
+            return date;
+        }
+
+        public void setDate(Date date)
+        {
+            this.date = date;
+        }
+
+
+        public ArrayList<Date> getArrayList()
+        {
+            return arrayList;
+        }
+
+        public void setArrayList(ArrayList<Date> lArrayList)
+        {
+            this.arrayList = lArrayList;
+        }
+
+        public LinkedList<String> getLinkedList()
+        {
+            return linkedList;
+        }
+
+        public void setLinkedList(LinkedList<String> lLinkedList)
+        {
+            this.linkedList = lLinkedList;
+        }
+
+        public LinkedHashMap<String, Date> getLinkedMap()
+        {
+            return linkedMap;
+        }
+
+        public void setLinkedMap(LinkedHashMap<String, Date> linkedMap)
+        {
+            this.linkedMap = linkedMap;
+        }
+    }
+
+    public MapperTest(String lName)
+    {
+        super(lName);
+    }
+
+    public void testIt()
+    {
+        try
+        {
+            MapperTest.TestBean lDuupje = new MapperTest.TestBean();
+            lDuupje.setIntegerMbr(new Integer(13));
+            lDuupje.setIntMbr(17);
+            lDuupje.setShortMbr(new Short((short) 17));
+            lDuupje.setStringMbr("It is not fair!");
+            lDuupje.setBooleanMbr(true);
+            lDuupje.setByteMbr((byte) 32);
+            lDuupje.setLongMbr(12345l);
+            lDuupje.setFloatMbr(123.12f);
+            lDuupje.setDoubleMbr(987.89);
+            lDuupje.setBigIntMbr(new BigInteger("123654789555"));
+            lDuupje.setBigDecimalMbr(new BigDecimal("1111111465465.676476545"));
+            lDuupje.setCharMbr('A');
+            lDuupje.setDate(new Date());
+                        
+            LinkedList lLinkedList = new LinkedList();
+            lLinkedList.add("uno");
+            lLinkedList.add("duo");
+            lDuupje.setLinkedList(lLinkedList);
+
+            ArrayList<Date> lArrayList = new ArrayList<Date>();
+            lArrayList.add(new Date());
+            lArrayList.add(new Date());
+            lDuupje.setArrayList(lArrayList);
+
+            LinkedHashMap<String, Date> lMap = new LinkedHashMap<String, Date>();
+            lMap.put("uno", new Date());
+            lMap.put("duo", new Date());
+            lDuupje.setLinkedMap(lMap);
+
+            JSONValue lObj = JSONMapper.toJSON(lDuupje);
+            System.out.println(lObj.render(true));
+
+            MapperTest.TestBean lLitmus = (MapperTest.TestBean) JSONMapper.toJava(lObj, TestBean.class);
+            Assert.assertNotNull(lLitmus);
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace(System.out);
+            Assert.fail();
+        }
+    }
+}
---
+++ tags/core-1.4/src/test/java/com/sdicons/json/serializer/UsersJavaBeanTest.java
@@ -0,0 +1,163 @@
+package com.sdicons.json.serializer;
+
+/*
+    JSONTools - Java JSON Tools
+    Copyright (C) 2006 S.D.I.-Consulting BVBA
+    http://www.sdi-consulting.com
+    mailto://nospam@sdi-consulting.com
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+import junit.framework.TestCase;
+import junit.framework.Assert;
+import com.sdicons.json.serializer.marshall.Marshall;
+import com.sdicons.json.serializer.marshall.JSONMarshall;
+import com.sdicons.json.serializer.marshall.MarshallValue;
+import com.sdicons.json.model.JSONObject;
+
+public class UsersJavaBeanTest
+extends TestCase
+{
+    public static class Transportable
+    {
+        private String param1;
+        private String param2;
+        private Integer objectID;
+        private Integer parentID;
+        private Integer subObjectID;
+        private Integer transportCode;
+        private Integer eventType;
+
+        public Integer getEventType()
+        {
+            return eventType;
+        }
+
+        public void setEventType(Integer eventType)
+        {
+            this.eventType = eventType;
+        }
+
+        public Integer getObjectID()
+        {
+            return objectID;
+        }
+
+        public void setObjectID(Integer objectID)
+        {
+            this.objectID = objectID;
+        }
+
+        public String getParam1()
+        {
+            return param1;
+        }
+
+        public void setParam1(String param1)
+        {
+            this.param1 = param1;
+        }
+
+        public String getParam2()
+        {
+            return param2;
+        }
+
+        public void setParam2(String param2)
+        {
+            this.param2 = param2;
+        }
+
+        public Integer getParentID()
+        {
+            return parentID;
+        }
+
+        public void setParentID(Integer parentID)
+        {
+            this.parentID = parentID;
+        }
+
+        public Integer getSubObjectID()
+        {
+            return subObjectID;
+        }
+
+        public void setSubObjectID(Integer subObjectID)
+        {
+            this.subObjectID = subObjectID;
+        }
+
+        public Integer getTransportCode()
+        {
+            return transportCode;
+        }
+
+        public void setTransportCode(Integer transportCode)
+        {
+            this.transportCode = transportCode;
+        }
+    }
+
+    public UsersJavaBeanTest(String lName)
+    {
+        super(lName);
+    }
+
+    Marshall marshall;
+
+    public void setUp()
+    throws Exception
+    {
+        marshall = new JSONMarshall();
+    }
+
+    public void testIt()
+    {
+        try
+        {
+            Transportable lEvil = new Transportable();
+            Integer lID = 13;
+            lEvil.setEventType(lID);
+            lEvil.setParentID(lID);
+            lEvil.setSubObjectID(lID);
+            lEvil.setObjectID(lID);
+            String lStr = "Test";
+            lEvil.setParam1(lStr);
+            lEvil.setParam2(lStr);
+
+            JSONObject lObj = marshall.marshall(lEvil);
+            System.out.println(lObj.render(true));
+            MarshallValue lResult = marshall.unmarshall(lObj);
+            Assert.assertTrue(MarshallValue.REFERENCE == lResult.getType());
+            Transportable lLitmus = (Transportable) lResult.getReference();
+
+            // Test if the contents are intact.
+            Assert.assertNotNull(lLitmus);
+            Assert.assertEquals(lLitmus.getEventType(), lID);
+            Assert.assertTrue(lLitmus.getEventType() == lLitmus.getParentID());
+            Assert.assertTrue(lLitmus.getEventType() == lLitmus.getSubObjectID());
+            Assert.assertTrue(lLitmus.getEventType()== lLitmus.getObjectID());
+            Assert.assertEquals(lLitmus.getParam1(), lStr);
+            Assert.assertTrue(lLitmus.getParam1() == lLitmus.getParam2());
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace(System.out);
+            Assert.fail();
+        }
+    }
+}

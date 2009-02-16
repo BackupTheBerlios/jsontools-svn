39c84c7330fdfd9d6904ded3c640c8f08e0eba5e
branscha <branscha@5ee35f29-4713-0410-8bc3-af33b2071d23>
2009-02-16 09:20:28 +0000

    Introduced ANTLR v3. This forced me to cha nge the interface of the parser, I introduced a new exception. Since this version will be incompatible (slightly different) with previous versions, a new major version will be needed.

--- trunk/jsontools-log4j/src/test/java/com/sdicons/json/log4j/LayoutTest.java
+++ trunk/jsontools-log4j/src/test/java/com/sdicons/json/log4j/LayoutTest.java
@@ -25,9 +25,10 @@ import com.sdicons.json.model.JSONObject;
 import com.sdicons.json.model.JSONString;
 import com.sdicons.json.model.JSONValue;
 import com.sdicons.json.parser.JSONParser;
-import com.sdicons.json.validator.Validator;
+import com.sdicons.json.parser.JSONParserException;
 import com.sdicons.json.validator.JSONValidator;
 import com.sdicons.json.validator.ValidationException;
+import com.sdicons.json.validator.Validator;
 import junit.framework.TestCase;
 import org.apache.log4j.ConsoleAppender;
 import org.apache.log4j.FileAppender;
@@ -38,9 +39,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.HashMap;
-
-import antlr.TokenStreamException;
-import antlr.RecognitionException;
 
 
 public class LayoutTest
@@ -53,90 +51,97 @@ extends TestCase
         try
         {
             lTempLog = File.createTempFile("json-log-test", ".txt");
-            lTempLog.createNewFile();
+            //lTempLog.createNewFile();
 
-            final Logger lLog = Logger.getLogger(LayoutTest.class);
-
-            final Layout lLayout = new JSONLayout();
-            final FileAppender lAppender = new FileAppender();
-            lAppender.setFile(lTempLog.getAbsolutePath());
-            lAppender.setLayout(lLayout);
-            lAppender.activateOptions();
-
-            final Logger lRootLogger = Logger.getRootLogger();
-            lRootLogger.addAppender(new ConsoleAppender(lLayout));
-            lRootLogger.addAppender(lAppender);
-
-            // Do some logging from the main thread.
-            lLog.info("This is an info message.");
-            lLog.debug("This is a debug message.");
-            lLog.error("This is an error message.");
-            lLog.fatal("This is a fatal message.", new IllegalArgumentException("This is an illegal argument."));
-
-            // Do some logging from helper threads.
-            for(int i = 0; i < 10; i++)
             {
-                new Thread(new Runnable()
+                final Logger lLog = Logger.getLogger(LayoutTest.class);
+
+                final Layout lLayout = new JSONLayout();
+                final FileAppender lAppender = new FileAppender();
+                lAppender.setFile(lTempLog.getAbsolutePath());
+                lAppender.setLayout(lLayout);
+                lAppender.activateOptions();
+
+                final Logger lRootLogger = Logger.getRootLogger();
+                lRootLogger.addAppender(new ConsoleAppender(lLayout));
+                lRootLogger.addAppender(lAppender);
+
+                // Do some logging from the main thread.
+                lLog.info("This is an info message.");
+                lLog.debug("This is a debug message.");
+                lLog.error("This is an error message.");
+                lLog.fatal("This is a fatal message.", new IllegalArgumentException("This is an illegal argument."));
+
+                // Do some logging from helper threads.
+                for(int i = 0; i < 10; i++)
                 {
-                    public void run()
+                    new Thread(new Runnable()
                     {
-                        lLog.info("This is an info message from a thread.");
-                        lLog.debug("This is a debug message from a thread.");
-                        lLog.error("This is an error message from a tread.");
-                    }
-                }).start();
-            }
-
-            // Wait some time for the threads to end ...
-            try { Thread.sleep(2000); } catch (InterruptedException e) { }
-
-            // Open the JSON log file.
-            FileInputStream lLogStream = new FileInputStream(lTempLog);
-            JSONParser lLogParser = new JSONParser(lLogStream);
-
-            // Initialize the validator of the log file contents.
-            final JSONParser lLogValidatorParser = new JSONParser(LayoutTest.class.getResourceAsStream("/log4j-validator.json"));
-            final Validator lLogValidator = new JSONValidator((JSONObject) lLogValidatorParser.nextValue());
-
-            // Initialize statistics.
-            int lCount = 0;
-            HashMap<String,int[]> lStats = new HashMap<String, int[]>();
-            lStats.put("INFO", new int[]{0});
-            lStats.put("ERROR", new int[]{0});
-            lStats.put("DEBUG", new int[]{0});
-            lStats.put("FATAL", new int[]{0});
-
-            // While info available in the stream (we take 1 because of the trailing newline).
-            while(lLogStream.available() > 1)
-            {
-                try
-                {
-                    JSONValue lVal = lLogParser.nextValue();
-                    lLogValidator.validate(lVal);
-                    
-                    JSONObject lLogObj = (JSONObject) lVal;
-                    String lLevel = ((JSONString) lLogObj.getValue().get("level")).getValue();
-                    if(lStats.containsKey(lLevel))
-                    {
-                        int[] lLevelCount = lStats.get(lLevel);
-                        lLevelCount[0]++;
-                    }
-                }
-                catch (Exception e)
-                {
-                    e.printStackTrace();
-                    TestCase.fail();
+                        public void run()
+                        {
+                            lLog.info("This is an info message from a thread.");
+                            lLog.debug("This is a debug message from a thread.");
+                            lLog.error("This is an error message from a tread.");
+                        }
+                    }).start();
                 }
 
-                lCount++;
+                // Wait some time for the threads to end ...
+                try { Thread.sleep(2000); } catch (InterruptedException e) { }
+
+                // Remove the appender and close the file.
+                lRootLogger.removeAppender(lAppender);
+                lAppender.close();
             }
 
-            // Test some stattistics.
-            TestCase.assertEquals(lCount, 34);
-            TestCase.assertEquals((lStats.get("ERROR"))[0], 11);
-            TestCase.assertEquals((lStats.get("INFO"))[0], 11);
-            TestCase.assertEquals((lStats.get("DEBUG"))[0], 11);
-            TestCase.assertEquals((lStats.get("FATAL"))[0], 1);
+            {
+                // Open the JSON log file.
+                final FileInputStream lLogStream = new FileInputStream(lTempLog);
+                final JSONParser lLogParser = new JSONParser(lLogStream);
+
+                // Initialize the validator of the log file contents.
+                final JSONParser lLogValidatorParser = new JSONParser(LayoutTest.class.getResourceAsStream("/log4j-validator.json"));
+                final Validator lLogValidator = new JSONValidator((JSONObject) lLogValidatorParser.nextValue());
+
+                // Initialize statistics.
+                int lCount = 0;
+                HashMap<String,int[]> lStats = new HashMap<String, int[]>();
+                lStats.put("INFO", new int[]{0});
+                lStats.put("ERROR", new int[]{0});
+                lStats.put("DEBUG", new int[]{0});
+                lStats.put("FATAL", new int[]{0});
+
+                // While info available in the stream (we take 1 because of the trailing newline).
+                while(true)
+                {
+                    try
+                    {
+                        JSONValue lVal = lLogParser.nextValue();
+                        lLogValidator.validate(lVal);
+
+                        JSONObject lLogObj = (JSONObject) lVal;
+                        String lLevel = ((JSONString) lLogObj.getValue().get("level")).getValue();
+                        if(lStats.containsKey(lLevel))
+                        {
+                            int[] lLevelCount = lStats.get(lLevel);
+                            lLevelCount[0]++;
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        break;
+                    }
+
+                    lCount++;
+                }
+
+                // Test some stattistics.
+                TestCase.assertEquals(lCount, 34);
+                TestCase.assertEquals((lStats.get("ERROR"))[0], 11);
+                TestCase.assertEquals((lStats.get("INFO"))[0], 11);
+                TestCase.assertEquals((lStats.get("DEBUG"))[0], 11);
+                TestCase.assertEquals((lStats.get("FATAL"))[0], 1);
+            }
 
         }
         catch (IOException e)
@@ -149,14 +154,9 @@ extends TestCase
             e.printStackTrace();
             TestCase.fail(e.getMessage());
         }
-        catch (TokenStreamException e)
+        catch(JSONParserException e)
         {
-            e.printStackTrace();
-            TestCase.fail(e.getMessage());
-        }
-        catch (RecognitionException e)
-        {
-            e.printStackTrace();
+             e.printStackTrace();
             TestCase.fail(e.getMessage());
         }
         finally
